2ab36f780812238a10f24af05de34d2645f6dd0f
Mike Higgins <mrmikehiggins@gmail.com>
2014-12-22 21:58:28 +0800

    tidied up naming of scripts

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 osmpgsql
 ========
+A simple shell script to get a PostgreSQL database up and running quickly with OSM data
+imported and ready to query.
 
-Import OSM data into Postgres DB
+===Method
+# Create a simple bash script to sequentially call other scripts to do the following:
+## 
+
+
+
--- osmpgsql.sh
+++ osmpgsql.sh
@@ -5,5 +5,9 @@ echo "- Download and install osm2pgsql"
 echo "- Download OSM data"
 echo "- Import OSM data into PGSQL DB"
 
-./scripts/create-pgdb.sh
+./scripts/01-create-pgdb.sh
 
+# Still testing the base install procedures
+# ./scripts/02-install-osm2pgsql.sh
+# ./scripts/03-download-osm-data.sh
+# ./scripts/04-import-osm-data.sh
