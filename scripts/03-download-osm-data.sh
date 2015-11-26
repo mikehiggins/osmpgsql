76b66a5f03ff88d4aaa12cdaeaffb7d917d2d204
David Higgins <Dave@albadevelopment.com.au>
2015-11-26 16:15:43 +1000

    Added Curl for Australia
    added Download link for australia-oceania/australia-latest.osm.pbf

--- scripts/03-download-osm-data.sh
+++ scripts/03-download-osm-data.sh
@@ -3,3 +3,4 @@
 # Download OSM data
 # no need for now, just use data in ./data/* directory
 #curl http://download.geofabrik.de/asia/china-latest.osm.pbf
+#curl http://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf
